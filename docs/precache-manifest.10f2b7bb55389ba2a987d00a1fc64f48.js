4d077f73e2023b9cee306bbb698530a0c5c80723
ywong <wongyok@gmail.com>
2020-01-02 15:00:06 +1100

    docs

---
+++ docs/precache-manifest.10f2b7bb55389ba2a987d00a1fc64f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6e020e63952c99d506413a22df3848",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b69baf5f5ba64d22a7cd",
+    "url": "/static/css/main.a0ba23e8.chunk.css"
+  },
+  {
+    "revision": "6b69daaf729194c1377f",
+    "url": "/static/js/2.bf6f3b9b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bf6f3b9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b69baf5f5ba64d22a7cd",
+    "url": "/static/js/main.6da2a07d.chunk.js"
+  },
+  {
+    "revision": "1b7a606e5d497235b882",
+    "url": "/static/js/runtime-main.168168f8.js"
+  }
+]);
